174b387818d70a4f967fac4ea33abf61a7f5e83f
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-10-22 23:21:11 -0600

    Completed

--- src/main/java/com/codindojo/Application.java
+++ src/main/java/com/codindojo/Application.java
@@ -2,12 +2,26 @@ package com.codindojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping( value = "/", method = RequestMethod.GET )
+	public String mainPage() {
+		return "<h1>Hello client! How are you doing?</h1>";
+	}
+	
+	@RequestMapping( value = "/random", method = RequestMethod.GET )
+	public String randomPage() {
+		return "<h1>Spring Boot is great! So easy to just respond with strings</h1>";
 	}
 
 }
